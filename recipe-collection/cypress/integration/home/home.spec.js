a73e9d91745bc43087e2a08780168a04052c4a6c
mbloom707 <mlbloom707@gmail.com>
2022-02-22 14:25:25 -0800

    Uploading files from 'Using TDD to Build a New Feature'

--- recipe-collection/cypress/integration/home/home.spec.js
+++ recipe-collection/cypress/integration/home/home.spec.js
@@ -8,4 +8,35 @@ describe("Home page", () => {
       .findByText('There are no recipes to list.')
       .should('exist')
   })
+  it("contains an add recipe button that when clicked opens a form", () => {
+    cy.findByRole('button').click();
+
+    cy.get('form')
+      .findByRole('button')
+      .should('exist')
+  })
+  it("displays a recipe name under the 'My Recipes' heading after it has been added through the 'Add Recipe' form", () => {
+    const recipeName = 'Tofu Scramble Tacos';
+    cy.findByRole('button').click()
+    cy.findByRole('textbox', {name: /Recipe name/i}).type(recipeName)
+    cy.findByRole('textbox', {name: /instructions/i}).type("1. heat a skillet on medium with a dollop of coconut oil {enter} 2. warm flour tortillas")
+
+    return cy.findByRole('button').click()
+      .then(() => {
+      expect(cy.findByRole('listitem', /tofu scramble tacos/i)).toExist();
+      })
+  })
+  it("adds multiple recipes", () => {
+    const recipeName = 'Cereal';
+    cy.findByRole('button').click()
+    cy.findByRole('textbox', {name: /Recipe name/i}).type(recipeName)
+    cy.findByRole('textbox', {name: /instructions/i}).type("1. Add cereal 2. Add Milk")
+
+    return cy.findByRole('button').click()
+      .then(() => {
+      expect(cy.findByRole('listitem', /tofu scramble tacos/i)).toExist()
+      expect(cy.findByRole('listitem', /cereal/i)).toExist();
+      })
+  })
 })
+
--- recipe-collection/src/App.test.js
+++ recipe-collection/src/App.test.js
@@ -1,8 +1,34 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const setup = () => {
+  const app = render(<App />);
+
+  userEvent.click(app.getByText('Add Recipe'));
+
+  // Add the submit button to your setup method:
+  const submitButton = app.getByRole('button')
+  const instructionsInput = app.getByLabelText('Instructions:')
+  const nameInput = app.getByLabelText('Recipe name:')
+
+  return {
+    instructionsInput,
+    nameInput,
+    submitButton
+  }
+}
+
+test('recipe name from state appears in an unordered list', async () => {
+  const {instructionsInput, nameInput, submitButton} = setup();
+  const recipeName = "Lean Pockets"
+  const recipeInstructions = "place in toaster oven on 350 for 45 minutes"
+
+  await userEvent.type(instructionsInput, recipeInstructions)
+  await userEvent.type(nameInput, recipeName)
+  userEvent.click(submitButton);
+
+  expect(screen.getByRole('listitem')).toBeInTheDocument();
+  expect(screen.getByText(recipeName)).toBeInTheDocument();
+})
